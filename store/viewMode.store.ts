8a977fcc384e2757eac232574d7edc5ba512ffbb
Alexey Dudin <aleshadudin53@gmail.com>
2024-07-29 15:33:23 +0300

    refactor(store/viewMode): добавить pageSize в состояние store
    - Константы GRID_PAGE_SIZE и LIST_PAGE_SIZE перемещены в состояние store
    - Добавлен getter для получения pageSize в зависимости от режима отображения
    - Обновлен интерфейс IState для включения pageSize

--- store/viewMode.store.ts
+++ store/viewMode.store.ts
@@ -2,16 +2,26 @@ export type ViewMode = 'grid' | 'list';
 
 interface IState {
 	viewMode: ViewMode;
+
+  // Количество новостей на страницу
+  gridPageSize: number; // в режиме grid
+  listPageSize: number; // в режиме list
+
 }
 
 const defaultValue: IState = {
-	viewMode: 'grid'
+	viewMode: 'grid',
+  gridPageSize: 4,
+  listPageSize: 3
 }
 
 export const useViewModeStore = defineStore('viewMode', {
 	state: () => defaultValue,
 	getters: {
-		get: state => state.viewMode
+		get: state => state.viewMode,
+    pageSize: (state) => state.viewMode === 'grid'
+    ? state.gridPageSize
+    : state.listPageSize
 	},
 	actions: {
 		change(viewMode: ViewMode) {
